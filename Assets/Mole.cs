cc4e142c1554cc02bba49c10ded336233faf7dc4
JoDalsgaard1 <94169630+JoDalsgaard1@users.noreply.github.com>
2021-12-03 16:10:15 +0100

    WhackAMole fixes animation and sound

--- Assets/Mole.cs
+++ Assets/Mole.cs
@@ -8,24 +8,66 @@ public class Mole : MonoBehaviour
     public int molePosition;
     private float moleTimer;
     public float totalMoleTime;
-    
+    public Animator mole_Animator;
     public FloatEvent globaltime;
+    public AudioSource exit;
+    public AudioSource hit;
+    private bool exitPlaying;
+    private Coroutine exitRoutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Spille animation af mole der kommer op
+        mole_Animator = GetComponent<Animator>();
+        exitPlaying = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         moleTimer += Time.deltaTime;
-        if (moleTimer >= totalMoleTime)
+        if (moleTimer >= totalMoleTime && exitPlaying == false) 
+        {
+            exitPlaying = true;
+            if (exitRoutine == null)
             {
-            // Spille animation af mole der går ned
+                exit.Play();
+                mole_Animator.SetTrigger("Exit");
+                exitRoutine = StartCoroutine(MoleDelay(true));
+            }
+        }
+    }
+
+    IEnumerator MoleDelay(bool delay)
+    {
+        while (true)
+        {
+            if (!delay)
+            {
+                ExitMole();
+                StopAllCoroutines();
+            }
+            delay = false;
+            yield return new WaitForSeconds(0.5f);
+
+        }
+    }
+
+    private void ExitMole()
+    {
             globaltime.Raise(-1);
             Destroy(this.gameObject);
+    }
+
+    public void MoleHit()
+    {
+        if (exitRoutine != null)
+        {
+            StopCoroutine(exitRoutine);
         }
+            hit.Play();
+            mole_Animator.SetTrigger("Hit");
+            globaltime.Raise(1);
+            Destroy(this.gameObject,1);
     }
 }
